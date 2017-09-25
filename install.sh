881a1a2739e22b816e188d274b06afdf8a4b34fc
Derrick Nelson <drrcknlsn@gmail.com>
2017-09-25 17:18:47 -0400

    Added Syntastic and polyglot. Fixed .vimrc symlink.

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ mkdir -p ~/.vim/bundle
 
 cd ~/.vim/bundle
 git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/sheerun/vim-polyglot.git
 git clone https://github.com/romainl/Apprentice.git
 
-ln -sv "~/.dotfiles/.vimrc" ~
+ln -sv ~/.dotfiles/.vimrc ~/.vimrc
