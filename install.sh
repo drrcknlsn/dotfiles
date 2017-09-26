a87e61fbbc28fe30ad664be43c92bea81176ca95
Derrick Nelson <drrcknlsn@gmail.com>
2017-09-26 08:54:33 -0400

    Added lightline.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cd ~/.vim/bundle
 git clone https://github.com/tpope/vim-pathogen.git
 git clone https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/sheerun/vim-polyglot.git
+git clone https://github.com/itchyny/lightline.vim.git
 git clone https://github.com/romainl/Apprentice.git
 
 ln -sv ~/.dotfiles/.vimrc ~/.vimrc
