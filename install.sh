9505fc860e8738dbddfb4042e35bbebefcc655ed
Derrick Nelson <drrcknlsn@gmail.com>
2017-10-06 23:03:51 -0400

    Added ALE. Added vim-surround. Added vim-visual-star-search. Removed Syntastic.

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 mkdir -p ~/.vim/bundle
 
 cd ~/.vim/bundle
-git clone https://github.com/tpope/vim-pathogen.git
-git clone https://github.com/vim-syntastic/syntastic.git
-git clone https://github.com/sheerun/vim-polyglot.git
+
 git clone https://github.com/itchyny/lightline.vim.git
+git clone https://github.com/nelstrom/vim-visual-star-search.git
 git clone https://github.com/romainl/Apprentice.git
+git clone https://github.com/sheerun/vim-polyglot.git
+git clone https://github.com/tpope/vim-pathogen.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/w0rp/ale.git
 
 ln -sv ~/.dotfiles/.vimrc ~/.vimrc
 
